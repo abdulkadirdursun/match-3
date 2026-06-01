81fc5bca5f6da382811c2884f5e95fda1423a664
Abdülkadir Dursun <akadirdursn@gmail.com>
2026-06-01 15:08:46 +0300

    feat: Create tile type data and prefab

---
+++ Assets/_Project/Core/Scripts/Tile.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace Match3.Core
+{
+    public class Tile : MonoBehaviour
+    {
+    }
+}
---
+++ Assets/_Project/Core/Scripts/TileTypeData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Match3.Core
+{
+    [CreateAssetMenu(fileName = "Tile Type", menuName = "Match 3/Core/Tile Type")]
+    public class TileTypeData : ScriptableObject
+    {
+        [SerializeField] private string tileName;
+        [SerializeField] private Tile prefab;
+
+        public string TileName => tileName;
+        public Tile Prefab => prefab;
+    }
+}
