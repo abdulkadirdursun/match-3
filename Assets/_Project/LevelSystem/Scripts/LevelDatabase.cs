9cc787131d58bf260a6b562659879f1fc3b51d15
Abdülkadir Dursun <akadirdursn@gmail.com>
2026-06-07 23:31:38 +0300

    feat: Create level database scriptable object

---
+++ Assets/_Project/LevelSystem/Scripts/LevelDatabase.cs
@@ -0,0 +1,19 @@
+﻿using Match3.Core.Helpers;
+using UnityEngine;
+
+namespace Match3.LevelSystem
+{
+    [CreateAssetMenu(fileName = "LevelDatabase", menuName = "Match 3/Level System/Level Database")]
+    public class LevelDatabase : ScriptableObject
+    {
+        [SerializeField] private LevelData[] levelDataArray;
+
+        public LevelData GetLoopLevelData(int level)
+        {
+            var levelIndex = level % levelDataArray.Length;
+            return levelDataArray[levelIndex];
+        }
+
+        public LevelData GetRandomLevelData() => levelDataArray.Random();
+    }
+}
