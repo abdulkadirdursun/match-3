07d30edfca9e41189da43f9ecf25e636a040a8a8
Abdülkadir Dursun <akadirdursn@gmail.com>
2026-06-08 16:43:03 +0300

    refactor: Spread additional weight data from array
    - since there are fixed number of neighbor, I created separate variables for each and used switch case to return the value, rather than looping all the values in the array everytime

--- Assets/_Project/Core/Scripts/BoardItems/ItemWeightData.cs
+++ Assets/_Project/Core/Scripts/BoardItems/ItemWeightData.cs
@@ -1,57 +1,52 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Match3.Core
 {
     [CreateAssetMenu(fileName = "ItemWeightData", menuName = "Match 3/Core/Item Weight Data")]
     public class ItemWeightData : ScriptableObject
     {
-        #region Structs
-
-        [Serializable]
-        public struct AdditionalWeightData
-        {
-            [SerializeField, Range(1, 8)] private int sameTypeNeighborCount;
-            [SerializeField, Range(-10f, 10f)] private float additionalWeight;
-
-            public int SameTypeNeighborCount
-            {
-                get => sameTypeNeighborCount;
-                set => sameTypeNeighborCount = value;
-            }
-            public float AdditionalWeight
-            {
-                get => additionalWeight;
-                set => additionalWeight = value;
-            }
-        }
-
-        #endregion
-
         [SerializeField, Range(0.1f, 20f)] private float baseWeight = 1f;
-        [SerializeField] private AdditionalWeightData[] additionalWeightData;
+        [Header("Additional Weight By Same Type Neighbor")]
+        [SerializeField, Range(-10f, 10f)] private float additionalWeightAtOneNeighbor;
+        [SerializeField, Range(-10f, 10f)] private float additionalWeightAtTwoNeighbor;
+        [SerializeField, Range(-10f, 10f)] private float additionalWeightAtThreeNeighbor;
+        [SerializeField, Range(-10f, 10f)] private float additionalWeightAtFourNeighbor;
+        [SerializeField, Range(-10f, 10f)] private float additionalWeightAtFiveNeighbor;
+        [SerializeField, Range(-10f, 10f)] private float additionalWeightAtSixNeighbor;
+        [SerializeField, Range(-10f, 10f)] private float additionalWeightAtSevenNeighbor;
+        [SerializeField, Range(-10f, 10f)] private float additionalWeightAtEightNeighbor;
 
         public float BaseWeight => baseWeight;
 
         public float GetAdditionalWeight(int sameTypeNeighborCount)
         {
-            AdditionalWeightData selectedWeightData = new();
-            foreach (ref readonly var weightData in additionalWeightData.AsSpan())
+            sameTypeNeighborCount = Mathf.Clamp(sameTypeNeighborCount, 0, 8);
+            return sameTypeNeighborCount switch
             {
-                if (weightData.SameTypeNeighborCount == sameTypeNeighborCount)
-                {
-                    return weightData.AdditionalWeight;
-                }
-
-                if (weightData.SameTypeNeighborCount > selectedWeightData.SameTypeNeighborCount
-                    && weightData.SameTypeNeighborCount < sameTypeNeighborCount)
-                {
-                    selectedWeightData.SameTypeNeighborCount = weightData.SameTypeNeighborCount;
-                    selectedWeightData.AdditionalWeight = weightData.AdditionalWeight;
-                }
-            }
-
-            return selectedWeightData.AdditionalWeight;
+                1 => additionalWeightAtOneNeighbor,
+                2 => additionalWeightAtTwoNeighbor,
+                3 => additionalWeightAtThreeNeighbor,
+                4 => additionalWeightAtFourNeighbor,
+                5 => additionalWeightAtFiveNeighbor,
+                6 => additionalWeightAtSixNeighbor,
+                7 => additionalWeightAtSevenNeighbor,
+                8 => additionalWeightAtEightNeighbor,
+                _ => 0f
+            };
         }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            additionalWeightAtOneNeighbor = Mathf.Max(-baseWeight, additionalWeightAtOneNeighbor);
+            additionalWeightAtTwoNeighbor = Mathf.Max(-baseWeight, additionalWeightAtTwoNeighbor);
+            additionalWeightAtThreeNeighbor = Mathf.Max(-baseWeight, additionalWeightAtThreeNeighbor);
+            additionalWeightAtFiveNeighbor = Mathf.Max(-baseWeight, additionalWeightAtFiveNeighbor);
+            additionalWeightAtOneNeighbor = Mathf.Max(-baseWeight, additionalWeightAtOneNeighbor);
+            additionalWeightAtSixNeighbor = Mathf.Max(-baseWeight, additionalWeightAtSixNeighbor);
+            additionalWeightAtSevenNeighbor = Mathf.Max(-baseWeight, additionalWeightAtSevenNeighbor);
+            additionalWeightAtEightNeighbor = Mathf.Max(-baseWeight, additionalWeightAtEightNeighbor);
+        }
+#endif
     }
 }
