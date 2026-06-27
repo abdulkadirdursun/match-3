e006fd705f2029fa660942576bcb9058f41ec1c2
Abdülkadir Dursun <akadirdursn@gmail.com>
2026-06-27 18:38:07 +0300

    feat: Add save system
    - No encryption

---
+++ Assets/_Project/SaveSystem/Scripts/BaseSavedScriptableObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Match3.SaveSystem
+{
+    public abstract class BaseSavedScriptableObject : ScriptableObject
+    {
+        public abstract void Initialize();
+        public abstract void Save();
+        public abstract void Load();
+        public abstract void Reset();
+    }
+}
---
+++ Assets/_Project/SaveSystem/Scripts/SavedObject.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace Match3.SaveSystem
+{
+    [Serializable]
+    public class SavedObject<T> where T : class
+    {
+        #region Constructor
+
+        public SavedObject(string fileName)
+        {
+            _filePath = Path.Combine(Application.persistentDataPath, $"{fileName}.akd");
+            if (File.Exists(_filePath)) LoadData();
+        }
+
+        #endregion
+
+        public T data;
+
+        private string _filePath;
+
+        public bool IsLoaded { get; private set; }
+
+        public void LoadData()
+        {
+            if (!File.Exists(_filePath)) return;
+            var json = File.ReadAllText(_filePath);
+            data = JsonUtility.FromJson<T>(json);
+            IsLoaded = true;
+        }
+
+        public void SaveData()
+        {
+            var json = JsonUtility.ToJson(data);
+            File.WriteAllText(_filePath, json);
+        }
+
+        public void Delete()
+        {
+            if (File.Exists(_filePath)) File.Delete(_filePath);
+        }
+
+        public T CopyData()
+        {
+            var json = JsonUtility.ToJson(data);
+            return JsonUtility.FromJson<T>(json);
+        }
+    }
+}
