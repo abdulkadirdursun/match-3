c3f4decee332ae3f838b83daee9cbb705d35cebb
Abdülkadir Dursun <akadirdursn@gmail.com>
2026-06-28 10:53:06 +0300

    remove: GenericSavedScriptableObject class

--- Assets/_Project/SaveSystem/Scripts/BaseSavedScriptableObject.cs
+++ Assets/_Project/SaveSystem/Scripts/BaseSavedScriptableObject.cs
@@ -9,4 +9,32 @@ namespace Match3.SaveSystem
         public abstract void Load();
         public abstract void Reset();
     }
+    
+    public class BaseSavedScriptableObject<T> : BaseSavedScriptableObject where T : class
+    {
+        [SerializeField] private SavedObject<T> defaultValues;
+
+        protected SavedObject<T> SavedObject { get; private set; }
+
+        public override void Initialize()
+        {
+            SavedObject = new SavedObject<T>(name);
+            if (SavedObject.IsLoaded) return;
+            SavedObject.data = defaultValues.CopyData();
+            SavedObject.SaveData();
+        }
+
+        public override void Save() => SavedObject?.SaveData();
+        public override void Load() => SavedObject?.LoadData();
+
+        public override void Reset()
+        {
+            SavedObject?.Delete();
+            SavedObject = new SavedObject<T>(name)
+            {
+                data = defaultValues.CopyData()
+            };
+            SavedObject.SaveData();
+        }
+    }
 }
